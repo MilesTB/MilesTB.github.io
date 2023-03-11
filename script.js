dc9890c7c6f733a3fa27caaccd6b0e86ef124c05
MilesTB <127628525+MilesTB@users.noreply.github.com>
2023-03-11 14:24:28 -0600

    Updated_website

---
+++ script.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("button1");
+button.addEventListener("click", function () {
+  alert("You have been Spidermaned");
+  setTimeout(function () {
+    location.href = "https://www.bing.com/videos/search?q=rick+roll+website+linl&&view=detail&mid=AFA128C39111665DD864AFA128C39111665DD864&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Drick%2Broll%2Bwebsite%2Blinl%26FORM%3DHDRSC4";
+  }),
+    3000;
+});
